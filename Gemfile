f3ea0d127b7e24f47250389c67691fd9743a432c
Developer <developer@example.com>
2026-06-28 14:06:43 +0530

    Add user table and user model

--- Gemfile
+++ Gemfile
@@ -42,10 +42,13 @@ gem "thruster", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
 
+# Bootstrap CSS and JavaScript framework
+gem "bootstrap"
+gem "dartsass-rails"
+
 group :development, :test do
   # Use byebug for interactive breakpoints in development and test.
   gem "byebug"
-
   # Audits gems for known security defects (use config/bundler-audit.yml to ignore issues)
   gem "bundler-audit", require: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_16_065150) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_28_081604) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "description"
     t.string "title"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.datetime "updated_at", null: false
+    t.string "username"
   end
 end
