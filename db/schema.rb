a902dcd8ed7f82c166672dfb6a8bceb1ccfff4fc
Developer <developer@example.com>
2026-06-28 18:42:56 +0530

    Add bcrypt gem for password encryption and implement secure password handling in User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_28_105101) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_28_125156) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "description"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_28_105101) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
+    t.string "password_digest"
     t.datetime "updated_at", null: false
     t.string "username"
   end
