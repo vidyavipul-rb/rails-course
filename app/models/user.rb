5247031453443500b2cb0c892f9e836a6c368037
Developer <developer@example.com>
2026-06-28 16:52:17 +0530

    Add one to many association for users and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+  has_many: true
+  validates :username, presence: true, 
+                       uniqueness: {case_sensitive: false}, 
+                       length: {minimum: 3, maximum: 25}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
